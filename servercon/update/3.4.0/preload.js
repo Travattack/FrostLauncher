c0fa6b8ba1cf0c6ae0dd3aaece5c0039019b788f
Travattack <joeri.pattyn@gmail.com>
2026-09-08 10:14:42 +0200

    Add files via upload

---
+++ servercon/update/3.4.0/preload.js
@@ -0,0 +1,25 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('launcherAPI', {
+  getConfig: () => ipcRenderer.invoke('get-config'),
+  browseExe: () => ipcRenderer.invoke('browse-exe'),
+  launchGame: (exePath) => ipcRenderer.invoke('launch-game', exePath),
+  setMusicMuted: (muted) => ipcRenderer.invoke('set-music-muted', muted),
+  getPatchnotes: () => ipcRenderer.invoke('get-patchnotes'),
+  checkNewPatchnotes: () => ipcRenderer.invoke('check-new-patchnotes'),
+  markPatchnotesRead: (hash) => ipcRenderer.invoke('mark-patchnotes-read', hash),
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  onUpdateStatus: (callback) => {
+    ipcRenderer.on('update-status', (_event, data) => callback(data));
+  },
+  onThemeAssets: (callback) => {
+    ipcRenderer.on('theme-assets', (_event, data) => callback(data));
+  },
+  repairLauncher: () => ipcRenderer.invoke('repair-launcher'),
+  checkServerStatus: () => ipcRenderer.invoke('check-server-status'),
+  onServerStatus: (callback) => {
+    ipcRenderer.on('server-status', (_event, data) => callback(data));
+  },
+  minimize: () => ipcRenderer.send('window-minimize'),
+  close: () => ipcRenderer.send('window-close'),
+});
